9dd64d296501329b1844d48293818a7053975ea9
Alex Felipe <alexfelipevieira@gmail.com>
2018-05-07 09:07:26 -0300

    vídeo 1.2

---
+++ app/src/main/java/br/com/alura/alurafood/MainActivity.java
@@ -0,0 +1,13 @@
+package br.com.alura.alurafood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
