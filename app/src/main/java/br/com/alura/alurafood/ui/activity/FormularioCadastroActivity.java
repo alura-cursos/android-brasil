b9df7998ca6a5debc6e9aadcfc146b8f6657e073
Alex Felipe <alexfelipevieira@gmail.com>
2018-05-07 10:06:04 -0300

    vídeo 1.3

---
+++ app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -0,0 +1,15 @@
+package br.com.alura.alurafood.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.com.alura.alurafood.R;
+
+public class FormularioCadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario_cadastro);
+    }
+}
