1115969450f63d46f966d39b71058c24ba60d46a
Alex Felipe <alexfelipevieira@gmail.com>
2018-05-08 10:03:46 -0300

    vídeo 2.2

--- app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
+++ app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -14,11 +14,37 @@ public class FormularioCadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_cadastro);
+
         TextInputLayout textInputNomeCompleto = findViewById(R.id.formulario_cadastro_campo_nome_completo);
         EditText campoNomeCompleto = textInputNomeCompleto.getEditText();
-        String nomeCompleto = campoNomeCompleto.getText().toString();
-        if(nomeCompleto.isEmpty()){
-            campoNomeCompleto.setError("Campo obrigatório");
-        }
+        adicionaValidacaoPadrao(campoNomeCompleto);
+
+        TextInputLayout textInputCpf = findViewById(R.id.formulario_cadastro_campo_cpf);
+        EditText campoCpf = textInputCpf.getEditText();
+        adicionaValidacaoPadrao(campoCpf);
+
+        TextInputLayout textInputTelefoneComDdd = findViewById(R.id.formulario_cadastro_campo_telefone_com_ddd);
+        EditText campoTelefoneComDdd = textInputTelefoneComDdd.getEditText();
+        adicionaValidacaoPadrao(campoTelefoneComDdd);
+
+        TextInputLayout textInputEmail = findViewById(R.id.formulario_cadastro_campo_email);
+        EditText campoEmail = textInputEmail.getEditText();
+        adicionaValidacaoPadrao(campoEmail);
+
+        TextInputLayout textInputSenha = findViewById(R.id.formulario_cadastro_campo_senha);
+        EditText campoSenha = textInputSenha.getEditText();
+        adicionaValidacaoPadrao(campoSenha);
+    }
+
+    private void adicionaValidacaoPadrao(final EditText campo) {
+        campo.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                String texto = campo.getText().toString();
+                if(texto.isEmpty()){
+                    campo.setError("Campo obrigatório");
+                }
+            }
+        });
     }
 }
