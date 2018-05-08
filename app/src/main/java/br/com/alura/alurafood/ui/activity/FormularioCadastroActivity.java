a724626b637348859c4c753a027f13731841b092
Alex Felipe <alexfelipevieira@gmail.com>
2018-05-08 09:40:05 -0300

    vídeo 2.1

--- app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
+++ app/src/main/java/br/com/alura/alurafood/ui/activity/FormularioCadastroActivity.java
@@ -1,7 +1,10 @@
 package br.com.alura.alurafood.ui.activity;
 
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import br.com.alura.alurafood.R;
 
@@ -11,5 +14,11 @@ public class FormularioCadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_cadastro);
+        TextInputLayout textInputNomeCompleto = findViewById(R.id.formulario_cadastro_campo_nome_completo);
+        EditText campoNomeCompleto = textInputNomeCompleto.getEditText();
+        String nomeCompleto = campoNomeCompleto.getText().toString();
+        if(nomeCompleto.isEmpty()){
+            campoNomeCompleto.setError("Campo obrigatório");
+        }
     }
 }
